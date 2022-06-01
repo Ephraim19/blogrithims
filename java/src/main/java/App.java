f5328ef05fc11fc36d2b06571abce68c2e650aed
Ephraim19 <machayoephraim@gmail.com>
2022-06-01 11:32:26 +0300

    first commit

---
+++ java/src/main/java/App.java
@@ -0,0 +1,13 @@
+public class App {
+
+    private static int add(int num1,int num2){
+        int ans = num1 + num2;
+        System.out.println(ans);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        add(13,17);
+    }
+
+}
---
+++ python/add.py
@@ -0,0 +1,8 @@
+def add(num1,num2):
+    ans = num1 + num2
+    print(ans)
+    return ans
+
+if __name__ == "__main__":
+    add(7,3)
+
